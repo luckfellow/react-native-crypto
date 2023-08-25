c836e432a38964cc18119cf2db4e598dbfa2653b
uijung.kim <luckfellow@gmail.com>
2023-08-25 18:09:51 +0900

    import randombytes 수정

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 'use strict'
 
-import { randomBytes } from 'react-native-get-random-values'
-exports.randomBytes = exports.rng = exports.pseudoRandomBytes = exports.prng = randomBytes
+//import { randomBytes } from 'react-native-get-random-values'
+//exports.randomBytes = exports.rng = exports.pseudoRandomBytes = exports.prng = randomBytes
+import 'react-native-get-random-values';
+exports.randomBytes = exports.rng = exports.pseudoRandomBytes = exports.prng = global.crypto.getRandomValues
 
 // implement window.getRandomValues(), for packages that rely on it
 if (typeof window === 'object') {
   if (!window.crypto) window.crypto = {}
   if (!window.crypto.getRandomValues) {
-    window.crypto.getRandomValues = function getRandomValues (arr) {
+    window.crypto.getRandomValues = function getRandomValues(arr) {
       let orig = arr
       if (arr.byteLength != arr.length) {
         // Get access to the underlying raw bytes
@@ -36,69 +38,69 @@ exports.pbkdf2 = p.pbkdf2
 exports.pbkdf2Sync = p.pbkdf2Sync
 
 var aes = require('browserify-cipher')
-;[
-  'Cipher',
-  'createCipher',
-  'Cipheriv',
-  'createCipheriv',
-  'Decipher',
-  'createDecipher',
-  'Decipheriv',
-  'createDecipheriv',
-  'getCiphers',
-  'listCiphers'
-].forEach(function (key) {
-  exports[key] = aes[key]
-})
+  ;[
+    'Cipher',
+    'createCipher',
+    'Cipheriv',
+    'createCipheriv',
+    'Decipher',
+    'createDecipher',
+    'Decipheriv',
+    'createDecipheriv',
+    'getCiphers',
+    'listCiphers'
+  ].forEach(function (key) {
+    exports[key] = aes[key]
+  })
 
 var dh = require('diffie-hellman')
-;[
-  'DiffieHellmanGroup',
-  'createDiffieHellmanGroup',
-  'getDiffieHellman',
-  'createDiffieHellman',
-  'DiffieHellman'
-].forEach(function (key) {
-  exports[key] = dh[key]
-})
+  ;[
+    'DiffieHellmanGroup',
+    'createDiffieHellmanGroup',
+    'getDiffieHellman',
+    'createDiffieHellman',
+    'DiffieHellman'
+  ].forEach(function (key) {
+    exports[key] = dh[key]
+  })
 
 var sign = require('browserify-sign')
-;[
-  'createSign',
-  'Sign',
-  'createVerify',
-  'Verify'
-].forEach(function (key) {
-  exports[key] = sign[key]
-})
+  ;[
+    'createSign',
+    'Sign',
+    'createVerify',
+    'Verify'
+  ].forEach(function (key) {
+    exports[key] = sign[key]
+  })
 
 exports.createECDH = require('create-ecdh')
 
 var publicEncrypt = require('public-encrypt')
 
-;[
-  'publicEncrypt',
-  'privateEncrypt',
-  'publicDecrypt',
-  'privateDecrypt'
-].forEach(function (key) {
-  exports[key] = publicEncrypt[key]
-})
+  ;[
+    'publicEncrypt',
+    'privateEncrypt',
+    'publicDecrypt',
+    'privateDecrypt'
+  ].forEach(function (key) {
+    exports[key] = publicEncrypt[key]
+  })
 
 var rf = require('randomfill')
 
 exports.randomFill = rf.randomFill
 exports.randomFillSync = rf.randomFillSync
 
-// the least I can do is make error messages for the rest of the node.js/crypto api.
-;[
-  'createCredentials'
-].forEach(function (name) {
-  exports[name] = function () {
-    throw new Error([
-      'sorry, ' + name + ' is not implemented yet',
-      'we accept pull requests',
-      'https://github.com/crypto-browserify/crypto-browserify'
-    ].join('\n'))
-  }
-})
+  // the least I can do is make error messages for the rest of the node.js/crypto api.
+  ;[
+    'createCredentials'
+  ].forEach(function (name) {
+    exports[name] = function () {
+      throw new Error([
+        'sorry, ' + name + ' is not implemented yet',
+        'we accept pull requests',
+        'https://github.com/crypto-browserify/crypto-browserify'
+      ].join('\n'))
+    }
+  })
